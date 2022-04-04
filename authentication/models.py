0c1ddc730e90a367eb37f9b4e4176179f2335183
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-04-04 13:31:30 +0200

    Add User model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+# pas d'username donc connexion par email ?
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["first_name", "last_name", "password"]
