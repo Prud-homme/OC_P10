d74d500f424a96752f35ca04b38ee35290aa8cfd
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-04-11 16:30:28 +0200

    Update settings and urls

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', #Cours toute seule ?
+    'rest_framework',
+    'rest_framework_simplejwt',
     'authentication',
     'softdesk_api',
 ]
@@ -127,3 +128,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=95),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
--- softdesk/urls.py
+++ softdesk/urls.py
@@ -15,11 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from softdesk_api.views import ProjectAPIView
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    #path('api-auth/', include('rest_framework.urls')),
-    path('projects/', ProjectAPIView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #path('signup/',),
+    #path('login/',),
+    path('projects/', ProjectAPIView.as_view(), name='projects'),
+    path('projects/<int:project_id>/', ProjectAPIView.as_view(), name='projects-details'),
+    #path('projects/<int:project_id>/users/',),
+    #path('projects/<int:project_id>/users/<int:user_id>/',),
+    #path('projects/<int:project_id>/issues/',),
+    #path('projects/<int:project_id>/issues/<int:issue_id>/',),
+    #path('projects/<int:project_id>/issues/<int:issue_id>/comments/',),
+    #path('projects/<int:project_id>/issues/<int:issue_id>/comments/<int:comment_id>/',),
 ]
