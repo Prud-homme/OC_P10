30876fd5c097d22704ecc6a5407065641e6c20a6
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-04-15 10:48:30 +0200

    Add serializer docstring of the Project model

--- softdesk_api/serializers/project.py
+++ softdesk_api/serializers/project.py
@@ -3,6 +3,10 @@ from rest_framework.serializers import ModelSerializer
 from softdesk_api.models import Project
  
 class ProjectSerializer(ModelSerializer):
+    """
+    Serializer of the Project model that restricts to the fields
+    id, title, description and type of project.
+    """
  
     class Meta:
         model = Project
