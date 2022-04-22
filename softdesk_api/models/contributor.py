81c3371a57222fff4ba915e7aeeb1f9fee577281
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-04-22 16:48:07 +0200

    Clean source code with flake8

--- softdesk_api/models/contributor.py
+++ softdesk_api/models/contributor.py
@@ -2,20 +2,21 @@ from django.db import models
 from django.conf import settings
 from .project import Project
 
-class Contributor(models.Model):
-	
-	# "Contributor" en fr ? 
-	CONTRIBUTOR = "CONT"
-	AUTHOR = "AUTH"
-	PERMISSION_CHOICES = [
-	    (CONTRIBUTOR, "Contributor"), 
-	    (AUTHOR, "Author"),
-	]
 
-	user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
-	permission = models.CharField(
+class Contributor(models.Model):
+
+    # "Contributor" en fr ?
+    CONTRIBUTOR = "CONT"
+    AUTHOR = "AUTH"
+    PERMISSION_CHOICES = [
+        (CONTRIBUTOR, "Contributor"),
+        (AUTHOR, "Author"),
+    ]
+
+    user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    permission = models.CharField(
         max_length=4,
         choices=PERMISSION_CHOICES,
     )
-	role = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
--- softdesk_api/models/project.py
+++ softdesk_api/models/project.py
@@ -3,25 +3,26 @@ from django.conf import settings
 
 
 class Project(models.Model):
-	"""
-	A project has 5 fields: id, title, description, project_type, author_user_id.
-	"""
-	BACKEND = "BCK"
-	FRONTEND = "FRT"
-	IOS = "IOS"
-	ANDROID = "ADR"
-	TYPE_CHOICES = [
-	    (BACKEND, "Back-end"), 
-	    (FRONTEND, "Front-end"),
-	    (IOS, "iOS"),
-	    (ANDROID, "Android"),
-	]
+    """
+    A project has 5 fields: id, title, description, project_type, author_user_id.
+    """
 
-	# project_id = models.IntegerField() = id dans la tablec
-	title = models.CharField(max_length=100)
-	description = models.CharField(max_length=250)
-	project_type = models.CharField(
+    BACKEND = "BCK"
+    FRONTEND = "FRT"
+    IOS = "IOS"
+    ANDROID = "ADR"
+    TYPE_CHOICES = [
+        (BACKEND, "Back-end"),
+        (FRONTEND, "Front-end"),
+        (IOS, "iOS"),
+        (ANDROID, "Android"),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    project_type = models.CharField(
         max_length=3,
         choices=TYPE_CHOICES,
     )
-	author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_user_id = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
