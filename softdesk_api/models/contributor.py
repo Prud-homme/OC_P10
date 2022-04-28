0495e26ed6f6a61a8774a044d0260f6e35e7b1e7
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-04-28 13:08:51 +0200

    Add Enum class for choices in model

--- softdesk_api/models/contributor.py
+++ softdesk_api/models/contributor.py
@@ -1,22 +1,27 @@
 from django.db import models
 from django.conf import settings
 from .project import Project
+from enum import Enum
+
+
+class Permission(Enum):
+    """Class that defines the different permissions"""
+
+    CONTRIBUTOR = "contributeur"
+    AUTHOR = "auteur"
+
+    @classmethod
+    def choices(cls):
+        """Returns a list of tuples representing the possible choices of permissions"""
+        return [(key.value, key.name) for key in cls]
 
 
 class Contributor(models.Model):
-
-    # "Contributor" en fr ?
-    CONTRIBUTOR = "CONT"
-    AUTHOR = "AUTH"
-    PERMISSION_CHOICES = [
-        (CONTRIBUTOR, "Contributor"),
-        (AUTHOR, "Author"),
-    ]
+    """
+    A issue has 4 fields: id, user_id, project_id, permission, role
+    """
 
     user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
-    permission = models.CharField(
-        max_length=4,
-        choices=PERMISSION_CHOICES,
-    )
+    permission = models.CharField(max_length=15, choices=Permission.choices())
     role = models.CharField(max_length=100)
--- softdesk_api/models/project.py
+++ softdesk_api/models/project.py
@@ -1,5 +1,22 @@
 from django.db import models
 from django.conf import settings
+from enum import Enum
+
+
+class ProjectType(Enum):
+    """Class that defines the different types of projects"""
+
+    BACKEND = "back-end"
+    FRONTEND = "front-end"
+    IOS = "iOS"
+    ANDROID = "Android"
+
+    @classmethod
+    def choices(cls):
+        """
+        Returns a list of tuples representing the possible choices of project types
+        """
+        return [(key.value, key.name) for key in cls]
 
 
 class Project(models.Model):
@@ -7,22 +24,9 @@ class Project(models.Model):
     A project has 5 fields: id, title, description, project_type, author_user_id.
     """
 
-    BACKEND = "BCK"
-    FRONTEND = "FRT"
-    IOS = "IOS"
-    ANDROID = "ADR"
-    TYPE_CHOICES = [
-        (BACKEND, "Back-end"),
-        (FRONTEND, "Front-end"),
-        (IOS, "iOS"),
-        (ANDROID, "Android"),
-    ]
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    project_type = models.CharField(
-        max_length=3,
-        choices=TYPE_CHOICES,
-    )
+    project_type = models.CharField(max_length=15, choices=ProjectType.choices())
     author_user_id = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
