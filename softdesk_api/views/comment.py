22c7ec6cf5c6171a7a5c00faf84754199fd79def
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-06-09 16:30:20 +0200

    Add post endpoint for comment

--- softdesk_api/views/comment.py
+++ softdesk_api/views/comment.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from softdesk_api.models import Comment
+from softdesk_api.models import Project, Issue, Comment
 from softdesk_api.serializers import CommentSerializer
 
 
@@ -25,21 +25,28 @@ class CommentAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request: HttpRequest) -> HttpResponse:
-        """
-        """
+        """ """
         pass
 
-    def post(self, request: HttpRequest) -> HttpResponse:
+    def post(
+        self, request: HttpRequest, project_id: int, issue_id: int
+    ) -> HttpResponse:
         """
+        Add the comment to the database if the information sent is valid.
         """
-        pass
+        project = Project.search_project(request, project_id)
+        issue = Issue.search_issue(request, project, issue_id)
+
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author_user_id=request.user, issue_id=issue)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request: HttpRequest) -> HttpResponse:
-        """
-        """
+        """ """
         pass
 
     def delete(self, request: HttpRequest) -> HttpResponse:
-        """
-        """
+        """ """
         pass
